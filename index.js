5a27e7d5bad30aea34ef01f9f82aea03143ad56d
mc6ril <cyril.lesot@yahoo.fr>
2021-07-09 14:17:55 +0200

    debug error

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const formidable = require('express-formidable');
-const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
 app.use(formidable());
-app.use(dotenv());
+app.use(cors());
 
+//Permet l'accès aux variables d'environnement
+require('dotenv').config();
 //Connection à mongoDb
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -20,11 +22,15 @@ const offerRoutes = require('./routes/offerRoutes');
 app.use(offerRoutes);
 
 //Route par défault :
+app.get('/', (req, res) => {
+    res.json("Bienvenue sur l'API de Vinted");
+});
+
 app.all('*', (req, res) => {
     res.status(404).json({ message: `Page not found` });
 });
 
 //Lancement du serveur
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server started');
 });
