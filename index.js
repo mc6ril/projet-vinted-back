4d9becc87635f25643497653ecde7e97425ca98d
mc6ril <cyril.lesot@yahoo.fr>
2021-07-09 14:49:55 +0200

    ok

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ mongoose.connect(process.env.MONGODB_URI, {
 //configuration de cloudinary :
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
-    cloud_name: CLOUD_NAME,
-    api_key: API_KEY,
-    api_secret: API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
     secure: true,
 });
 
